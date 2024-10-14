dad6f36372cc11c400a2d7f047f56142ed360de3
Rizwaan Salim <argenspin@gmail.com>
2024-10-14 21:13:58 +0530

    added base files

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': "Tiju's Custom Dashboards",
+    'author': 'Rizwaan',
+    'version': "17.0.0.0",
+    'sequence': "0",
+    'depends': ['base','web'],
+    'data': [
+    ],
+    'demo': [],
+    'summary': "Tiju's Custom Dashboards",
+    'description': "Tiju's Custom Dashboards",
+    'installable': True,
+    'auto_install': False,
+    'license': "LGPL-3",
+    'application': False
+}
